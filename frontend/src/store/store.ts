59937453b21bcc0f1becb2bdacb52eb6c86a3891
Bryan Chen <bryanchen74@gmail.com>
2020-11-21 00:56:41 +0000

    Always enable devtools

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -46,7 +46,6 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(socketIOMiddleware),
-  devTools: !production,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
