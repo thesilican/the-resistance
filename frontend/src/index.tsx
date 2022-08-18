16df7e68df47fe290996f4e07e1fc505be90fad4
Bryan Chen <bryanchen74@gmail.com>
2022-08-17 21:03:23 -0400

    Add basename to React Router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
